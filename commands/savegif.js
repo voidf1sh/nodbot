2e71b114f2851bb7029bb0488242bc19c8f06e05
= <official.xian+git@gmail.com>
2021-07-19 16:01:57 -0400

    ability to cancel savegif

--- commands/savegif.js
+++ commands/savegif.js
@@ -6,7 +6,7 @@ const tenor = require('tenorjs').client({
     "DateFormat": "D/MM/YYYY - H:mm:ss A" // Change this accordingly
 });
 const functions = require('../functions');
-const { emoji } = require('../config.json');
+const { emoji } = require('../src/strings.json');
 
 module.exports = {
 	name: 'savegif',
@@ -32,15 +32,17 @@ module.exports = {
 				// Send the first GIF result as an Embed
 				channel.send(embed)
 					.then(selfMessage => {
-						// Add reactions to go back, forward, and confirm GIF choice.
+						// Add reactions to go back, forward, cancel and confirm GIF choice.
 						// React order is important so these are done in a chain
 						selfMessage.react(emoji.previous).then(() => {
 							selfMessage.react(emoji.confirm).then(() => {
-								selfMessage.react(emoji.next);
+								selfMessage.react(emoji.next).then(() => {
+									selfMessage.react(emoji.cancel);
+								});
 							});
 						});
 						const filter = (reaction, user) => {
-							return ((reaction.emoji.name == emoji.next) || (reaction.emoji.name == emoji.confirm) || (reaction.emoji.name == emoji.previous)) && user.id == message.author.id;
+							return ((reaction.emoji.name == emoji.next) || (reaction.emoji.name == emoji.confirm) || (reaction.emoji.name == emoji.previous) || (reaction.emoji.name == emoji.cancel)) && user.id == message.author.id;
 						}
 						const collector = selfMessage.createReactionCollector(filter, { time: 120000 });
 
@@ -60,8 +62,19 @@ module.exports = {
 									}
 									break;
 								case emoji.confirm:
-									channel.send('GIF Selected. What should I save the GIF as? (don\'t include the `.gif`)')
+									channel.send('GIF Selected. What should I save the GIF as? (don\'t include the `.gif`)\nReact with ' + emoji.cancel + ' to cancel.')
 										.then(nameQueryMessage => {
+											nameQueryMessage.react(emoji.cancel);
+											const cancelReactFilter = (reaction, user) => {
+												return (reaction.emoji.name == emoji.cancel) && (user.id == message.author.id);
+											}
+											const cancelReactCollector = nameQueryMessage.createReactionCollector(cancelReactFilter, { time: 20000, max: 1 });
+
+											cancelReactCollector.on('collect', (reaction, user) => {
+												nameCollector.stop('cancel');
+												if (selfMessage.deletable) selfMessage.delete();
+												if (nameQueryMessage.deletable) nameQueryMessage.delete();
+											})
 											const nameCollectorFilter = nameMessage => nameMessage.author == message.author;
 											const nameCollector = nameQueryMessage.channel.createMessageCollector(nameCollectorFilter, { time: 30000, max: 1 });
 
@@ -69,7 +82,17 @@ module.exports = {
 												channel.send('The GIF has been saved as: ' + nameMessage.content + '.gif');
 												functions.saveGif(message, nameMessage.content, data.embed_url);
 											});
+											nameCollector.on('end', (collected, reason) => {
+												switch (reason) {
+													case 'cancel':
+														channel.send('The action has been canceled.');
+														break;
+												default:
+														break;
+												}
+											});
 										});
+									collector.stop("confirm");
 									break;
 								case emoji.previous:
 									if (i > 0) {
@@ -84,8 +107,24 @@ module.exports = {
 										selfMessage.edit(embed);
 									}
 									break;
+								case emoji.cancel:
+									collector.stop('cancel');
+									break;
 								default:
 									channel.send('There was an error, sorry.');
+									break;
+							}
+						});
+
+						collector.on('end', (collected, reason) => {
+							switch (reason) {
+								case 'cancel':
+									selfMessage.delete();
+									channel.send('The action has been canceled.');
+									break;
+								case 'messageDelete':
+									break;
+								default:
 									break;
 							}
 						})
---
+++ gifs/test.js
@@ -0,0 +1,4 @@
+module.exports = {
+	name: 'test',
+	embed_url: 'https://media.tenor.com/images/0f097ed319d498c2bda3d87ba4f6ff10/tenor.gif'
+}
