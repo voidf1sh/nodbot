96d6ca453927f916db6f68d0106d27c21e0045df
= <official.xian+git@gmail.com>
2021-07-20 16:32:16 -0400

    Cleaning input

--- commands/roll.js
+++ commands/roll.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
 const strings = require('../src/strings.json');
+const functions = require('../functions.js');
 
 module.exports = {
 	name: 'roll',
 	description: 'Add a phrase to the .joint command',
 	usage: '<phrase to save>',
 	execute(message, file) {
-		strings.weed.push(file.name);
+		strings.weed.push(functions.cleanInput(file.name));
 		fs.writeFile('./src/strings.json', JSON.stringify(strings), err => {
 			if (err) throw err;
 
