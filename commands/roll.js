a2ef26e9e325b4fdad0c4cedfa758ec1aa9ae69f
= <official.xian+git@gmail.com>
2021-07-20 16:24:24 -0400

    add usage

--- commands/roll.js
+++ commands/roll.js
@@ -4,6 +4,7 @@ const strings = require('../src/strings.json');
 module.exports = {
 	name: 'roll',
 	description: 'Add a phrase to the .joint command',
+	usage: '<phrase to save>',
 	execute(message, file) {
 		strings.weed.push(file.name);
 		fs.writeFile('./src/strings.json', JSON.stringify(strings), err => {
