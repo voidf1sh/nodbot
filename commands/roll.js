41175c0ffd0c316278f7142391e558f3cea29294
= <official.xian+git@gmail.com>
2021-07-20 16:23:30 -0400

    Ability to save phrases to .joint

---
+++ commands/roll.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const strings = require('../src/strings.json');
+
+module.exports = {
+	name: 'roll',
+	description: 'Add a phrase to the .joint command',
+	execute(message, file) {
+		strings.weed.push(file.name);
+		fs.writeFile('./src/strings.json', JSON.stringify(strings), err => {
+			if (err) throw err;
+
+			message.channel.send('It has been added to the list');
+		})
+	}
+}
