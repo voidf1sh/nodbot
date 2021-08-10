28a8c5346c1ae59b1f0d0ae1850c14377555fb3e
= <official.xian+git@gmail.com>
2021-08-10 15:34:11 -0400

    Include request in-channel

--- commands/request.js
+++ commands/request.js
@@ -6,7 +6,7 @@ module.exports = {
 	usage: '<request or feedback>',
 	execute(message, file) {
 		const request = file.name;
-		message.channel.send(fn.textEmbed('Your request has been submitted!', message.author, file.extension));
+		message.channel.send(fn.textEmbed('Your request has been submitted!\nRequest: ' + request, message.author, file.extension));
 		message.client.users.fetch(process.env.ownerID).then(user => {user.send(fn.textEmbed(request, message.author, file.extension));}).catch(error => { console.error(error);} );
 		fn.uploadRequest(message.author, file.name);
 	}
