3d64bf912ca7733729416d0be2bdfba20d5191e6
Skylar Grant <official.xian@gmail.com>
2022-06-11 10:55:25 -0400

    added sb alias

--- dot-commands/spongebob.js
+++ dot-commands/spongebob.js
@@ -3,9 +3,10 @@ const config = require('../config.json');
 
 module.exports = {
 	name: 'spongebob',
+	alias: 'sb',
 	description: 'SpOnGeBoB-iFy AnYtHiNg AuToMaTiCaLly',
 	usage: '<text to convert>.spongebob',
 	execute(message, commandData) {
-		message.reply(`@${message.author.username}: ${fn.spongebob(commandData)}`);
+		message.reply(fn.spongebob(commandData));
 	}
 }
