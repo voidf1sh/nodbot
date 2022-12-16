1da220c5a8f37c2bae3e942ee2ff9e588972384b
Skylar Grant <official.xian@gmail.com>
2022-12-16 14:56:00 -0500

    Improving Spongebob

--- functions.js
+++ functions.js
@@ -506,31 +506,28 @@ const functions = {
 		}
 	},
 	spongebob(commandData) {
-		let flipper = 0;
 		let newText = '';
 		for (const letter of commandData.args) {
 			if (letter == ' ') {
-				newText = newText + letter;
+				newText += letter;
 				continue;
 			}
 			if (letter == 'i' || letter == 'I') {
-				newText = newText + 'i';
+				newText += 'i';
 				continue;
 			}
 			if (letter == 'l' || letter == 'L') {
-				newText = newText + 'L';
+				newText += 'L';
 				continue;
 			}
-			if (flipper == 0) {
-				newText = newText + letter.toUpperCase();
-				flipper = 1;
+			if (Math.random() > 0.5) {
+				newText += letter.toUpperCase();
 			} else {
-				newText = newText + letter;
-				flipper = 0;
+				newText += letter.toLowerCase();
 			}
 		}
 
-		return newText;
+		return newText + ' <:spongebob:1053399026709569536>';
 	},
 };
 
