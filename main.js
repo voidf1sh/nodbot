57238ce57a975f07be8ac6eddc5977ad11707380
Skylar Grant <official.xian@gmail.com>
2023-04-13 20:16:33 -0400

    Add variety to ong

--- main.js
+++ main.js
@@ -197,7 +197,10 @@ client.on('messageCreate', message => {
 	const lowerContent = message.content.toLowerCase();
 	if (lowerContent.includes('big') && lowerContent.includes('doinks')) message.reply('gang.');
 	if (lowerContent.includes('ligma')) message.reply('ligma balls, goteem');
-	if (lowerContent.includes('frfr') || lowerContent.includes('fr fr') || lowerContent.includes('bussin') || lowerContent.includes(' ong') || lowerContent.startsWith('ong')) message.reply('ongggg no :billed_cap: fr fr str8 bussin'); 
+	if (lowerContent.includes('frfr') || lowerContent.includes('fr fr') || lowerContent.includes('bussin') || lowerContent.includes(' ong') || lowerContent.startsWith('ong')) {
+		const randIndex = Math.floor(Math.random() * strings.capbacks.length);
+		message.reply(strings.capbacks[randIndex]);
+	}
 
 	// Break the message down into its components and analyze it
 	const commandData = fn.dot.getCommandData(message);
